f762abd58390890aec95a1560a67453f1b6abf1f
Michael Iedema <mriedema@gmail.com>
2007-03-09 15:13:21 +0000

    shibby
    git-svn-id: https://wush.net/svn/askozia/trunk@17 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- m0n0base/webgui/system.php
+++ m0n0base/webgui/system.php
@@ -143,7 +143,6 @@ if ($_POST) {
 			$retval |= system_hosts_generate();
 			$retval |= system_resolvconf_generate();
 			$retval |= system_password_configure();
-			$retval |= services_dnsmasq_configure();
 			$retval |= system_timezone_configure();
  			$retval |= system_ntp_configure();
  			
