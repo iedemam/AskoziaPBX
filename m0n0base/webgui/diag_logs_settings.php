4e9f2b8ca80469cd7f0345d5219e48a8d8892310
Michael Iedema <mriedema@gmail.com>
2007-04-24 11:22:49 +0000

    remote syslogging works again
    git-svn-id: https://wush.net/svn/askozia/trunk@63 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- m0n0base/webgui/diag_logs_settings.php
+++ m0n0base/webgui/diag_logs_settings.php
@@ -36,6 +36,8 @@ $pconfig['reverse'] = isset($config['syslog']['reverse']);
 $pconfig['nentries'] = $config['syslog']['nentries'];
 $pconfig['remoteserver'] = $config['syslog']['remoteserver'];
 $pconfig['system'] = isset($config['syslog']['system']);
+$pconfig['asterisk'] = isset($config['syslog']['asterisk']);
+$pconfig['cdr'] = isset($config['syslog']['cdr']);
 $pconfig['enable'] = isset($config['syslog']['enable']);
 
 if (!$pconfig['nentries'])
@@ -59,6 +61,8 @@ if ($_POST) {
 		$config['syslog']['nentries'] = (int)$_POST['nentries'];
 		$config['syslog']['remoteserver'] = $_POST['remoteserver'];
 		$config['syslog']['system'] = $_POST['system'] ? true : false;
+		$config['syslog']['asterisk'] = $_POST['asterisk'] ? true : false;
+		$config['syslog']['cdr'] = $_POST['cdr'] ? true : false;
 		$config['syslog']['enable'] = $_POST['enable'] ? true : false;
 		
 		write_config();
@@ -81,9 +85,13 @@ function enable_change(enable_over) {
 	if (document.iform.enable.checked || enable_over) {
 		document.iform.remoteserver.disabled = 0;
 		document.iform.system.disabled = 0;
+		document.iform.asterisk.disabled = 0;
+		document.iform.cdr.disabled = 0;
 	} else {
 		document.iform.remoteserver.disabled = 1;
 		document.iform.system.disabled = 1;
+		document.iform.asterisk.disabled = 1;
+		document.iform.cdr.disabled = 1;
 	}
 }
 // -->
@@ -91,64 +99,80 @@ function enable_change(enable_over) {
 <form action="diag_logs_settings.php" method="post" name="iform" id="iform">
 <?php if ($input_errors) print_input_errors($input_errors); ?>
 <?php if ($savemsg) print_info_box($savemsg); ?>
+
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
-  <tr><td class="tabnavtbl">
-  <ul id="tabnav">
-<?php 
+	<tr>
+		<td class="tabnavtbl">
+			<ul id="tabnav"><?php 
+
 	$tabs = array('System' => 'diag_logs.php',
-				  'Asterisk' => 'diag_logs_asterisk.php',
-				  'Calls' => 'diag_logs_calls.php',
-	       		  'Settings' => 'diag_logs_settings.php');
+					'Asterisk' => 'diag_logs_asterisk.php',
+					'Calls' => 'diag_logs_calls.php',
+					'Settings' => 'diag_logs_settings.php');
 	dynamic_tab_menu($tabs);
 	
-?> 
-  </ul>
-  </td></tr>
-  <tr> 
-    <td class="tabcont">
-	  <table width="100%" border="0" cellpadding="6" cellspacing="0">
-                      <tr> 
-                        <td width="22%" valign="top" class="vtable">&nbsp;</td>
-                        <td width="78%" class="vtable"> <input name="reverse" type="checkbox" id="reverse" value="yes" <?php if ($pconfig['reverse']) echo "checked"; ?>>
-                          <strong>Show log entries in reverse order (newest entries 
-                          on top)</strong></td>
-                      </tr>
-                      <tr> 
-                        <td width="22%" valign="top" class="vtable">&nbsp;</td>
-                        <td width="78%" class="vtable">Number of log entries to 
-                          show: 
-                          <input name="nentries" id="nentries" type="text" class="formfld" size="4" value="<?=htmlspecialchars($pconfig['nentries']);?>"></td>
-                      </tr>
-                      <tr> 
-                        <td width="22%" valign="top" class="vtable">&nbsp;</td>
-                        <td width="78%" class="vtable"> <input name="enable" type="checkbox" id="enable" value="yes" <?php if ($pconfig['enable']) echo "checked"; ?> onClick="enable_change(false)">
-                          <strong>Enable syslog'ing to remote syslog server</strong></td>
-                      </tr>
-                      <tr> 
-                        <td width="22%" valign="top" class="vncell">Remote syslog 
-                          server</td>
-                        <td width="78%" class="vtable"> <input name="remoteserver" id="remoteserver" type="text" class="formfld" size="20" value="<?=htmlspecialchars($pconfig['remoteserver']);?>"> 
-                          <br>
-                          IP address of remote syslog server<br> <br>
-						  <input name="system" id="system" type="checkbox" value="yes" onclick="enable_change(false)" <?php if ($pconfig['system']) echo "checked"; ?>>
-                          system events</td>
-                      </tr>
-                      <tr> 
-                        <td width="22%" valign="top">&nbsp;</td>
-                        <td width="78%"> <input name="Submit" type="submit" class="formbtn" value="Save" onclick="enable_change(true)"> 
-                        </td>
-                      </tr>
-                      <tr> 
-                        <td width="22%" valign="top">&nbsp;</td>
-                        <td width="78%"><strong><span class="red">Note:</span></strong><br>
-                          syslog sends UDP datagrams to port 514 on the specified 
-                          remote syslog server. Be sure to set syslogd on the 
-                          remote server to accept syslog messages from AskoziaPBX. 
-                        </td>
-                      </tr>
-                    </table>
-    </td>
-  </tr>
+			?></ul>
+		</td>
+	</tr>
+	<tr> 
+		<td class="tabcont">
+			<table width="100%" border="0" cellpadding="6" cellspacing="0">
+				<tr> 
+					<td width="22%" valign="top" class="vtable">&nbsp;</td>
+					<td width="78%" class="vtable">
+						<input name="reverse" type="checkbox" id="reverse" value="yes" <?php if ($pconfig['reverse']) echo "checked"; ?>>
+						<strong>Show log entries in reverse order (newest entries 
+						on top)</strong>
+					</td>
+				</tr>
+				<tr> 
+					<td width="22%" valign="top" class="vtable">&nbsp;</td>
+					<td width="78%" class="vtable">
+						Number of log entries to show: 
+						<input name="nentries" id="nentries" type="text" class="formfld" size="4" value="<?=htmlspecialchars($pconfig['nentries']);?>">
+					</td>
+				</tr>
+				<tr> 
+					<td width="22%" valign="top" class="vtable">&nbsp;</td>
+					<td width="78%" class="vtable">
+						<input name="enable" type="checkbox" id="enable" value="yes" <?php if ($pconfig['enable']) echo "checked"; ?> onClick="enable_change(false)">
+						<strong>Enable syslog'ing to remote syslog server</strong>
+					</td>
+				</tr>
+				<tr> 
+					<td width="22%" valign="top" class="vncell">Remote syslog server</td>
+					<td width="78%" class="vtable">
+						<input name="remoteserver" id="remoteserver" type="text" class="formfld" size="20" value="<?=htmlspecialchars($pconfig['remoteserver']);?>"> 
+						<br>
+						IP address of remote syslog server
+						<br>
+						<br>
+						<input name="system" id="system" type="checkbox" value="yes" onclick="enable_change(false)" <?php if ($pconfig['system']) echo "checked"; ?>>system events
+						<br>
+						<input name="asterisk" id="asterisk" type="checkbox" value="yes" onclick="enable_change(false)" <?php if ($pconfig['asterisk']) echo "checked"; ?>>asterisk events
+						<br>
+						<input name="cdr" id="cdr" type="checkbox" value="yes" onclick="enable_change(false)" <?php if ($pconfig['cdr']) echo "checked"; ?>>call detail records
+					</td>
+				</tr>
+				<tr> 
+					<td width="22%" valign="top">&nbsp;</td>
+					<td width="78%">
+						<input name="Submit" type="submit" class="formbtn" value="Save" onclick="enable_change(true)"> 
+					</td>
+				</tr>
+				<tr> 
+					<td width="22%" valign="top">&nbsp;</td>
+					<td width="78%">
+						<strong><span class="red">Note:</span></strong>
+						<br>
+						syslog sends UDP datagrams to port 514 on the specified 
+						remote syslog server. Be sure to set syslogd on the 
+						remote server to accept syslog messages from AskoziaPBX. 
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
 </table>
 </form>
 <script language="JavaScript">
