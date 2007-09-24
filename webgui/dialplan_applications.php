b1f0308c01220109a917b4e8e8686f5274464c79
Michael Iedema <mriedema@gmail.com>
2007-09-24 22:36:42 +0000

    added application mapper
    git-svn-id: https://wush.net/svn/askozia/trunk@204 c9cd69a0-eb2a-0410-8ff2-b9414780476a

---
+++ webgui/dialplan_applications.php
@@ -0,0 +1,102 @@
+#!/usr/local/bin/php
+<?php 
+/*
+	$Id: dialplan_callgroups.php 198 2007-09-17 14:11:18Z michael.iedema $
+	part of AskoziaPBX (http://askozia.com/pbx)
+	
+	Copyright (C) 2007 IKT <http://itison-ikt.de>.
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+	
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+	
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+$pgtitle = array("Dialplan", "Applications");
+require("guiconfig.inc");
+
+// XXX this is_array, sort, reference stuff is all over...
+if (!is_array($config['dialplan']['application']))
+	$config['dialplan']['application'] = array();
+
+dialplan_sort_applications();
+$a_applications = &$config['dialplan']['application'];
+
+
+if ($_GET['act'] == "del") {
+	if ($a_applications[$_GET['id']]) {
+
+		unset($a_applications[$_GET['id']]);
+		
+		write_config();
+		touch($d_extensionsconfdirty_path);
+		header("Location: dialplan_applications.php");
+		exit;
+	}
+}
+
+if (file_exists($d_extensionsconfdirty_path)) {
+	$retval = 0;
+	if (!file_exists($d_sysrebootreqd_path)) {
+		config_lock();
+		$retval |= extensions_conf_generate();
+		config_unlock();
+		
+		$retval |= extensions_reload();
+	}
+	
+	$savemsg = get_std_save_message($retval);
+	if ($retval == 0) {
+		unlink($d_extensionsconfdirty_path);
+	}
+}
+
+?>
+
+<?php include("fbegin.inc"); ?>
+<form action="dialplan_applications.php" method="post">
+<?php if ($savemsg) print_info_box($savemsg); ?>
+
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td width="15%" class="listhdrr">Extension</td>
+		<td width="15%" class="listhdrr">Name</td>
+		<td width="60%" class="listhdrr">Description</td>
+		<td width="10%" class="list"></td>
+	</tr>
+	
+
+	<?php $i = 0; foreach ($a_applications as $app): ?>
+	<tr>
+		<td class="listlr"><?=htmlspecialchars($app['extension']);?></td>
+		<td class="listbg"><?=htmlspecialchars($app['name']);?>&nbsp;</td>
+		<td class="listr"><?=htmlspecialchars($applications[$app['name']]);?>&nbsp;</td>
+		<td valign="middle" nowrap class="list"> <a href="dialplan_applications_edit.php?id=<?=$i;?>"><img src="e.gif" title="edit application mapping" width="17" height="17" border="0"></a>
+           &nbsp;<a href="dialplan_applications.php?act=del&id=<?=$i;?>" onclick="return confirm('Do you really want to delete this application mapping?')"><img src="x.gif" title="delete application mapping" width="17" height="17" border="0"></a></td>
+	</tr>
+	<?php $i++; endforeach; ?>
+
+	<tr> 
+		<td class="list" colspan="3"></td>
+		<td class="list"> <a href="dialplan_applications_edit.php"><img src="plus.gif" title="add application mapping" width="17" height="17" border="0"></a></td>
+	</tr>
+</table>
+</form>
+<?php include("fend.inc"); ?>
---
+++ webgui/dialplan_applications_edit.php
@@ -0,0 +1,134 @@
+#!/usr/local/bin/php
+<?php 
+/*
+	$Id: dialplan_callgroups_edit.php 198 2007-09-17 14:11:18Z michael.iedema $
+	part of AskoziaPBX (http://askozia.com/pbx)
+	
+	Copyright (C) 2007 IKT <http://itison-ikt.de>.
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+	
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+	
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+require_once("functions.inc");
+
+$pgtitle = array("Dialplan", "Applications", "Edit");
+require("guiconfig.inc");
+
+// XXX this is_array, sort, reference stuff is all over...
+if (!is_array($config['dialplan']['application']))
+	$config['dialplan']['application'] = array();
+
+dialplan_sort_applications();
+$a_applications = &$config['dialplan']['application'];
+
+$id = $_GET['id'];
+if (isset($_POST['id']))
+	$id = $_POST['id'];
+
+/* pull current config into pconfig */
+if (isset($id) && $a_applications[$id]) {
+	$pconfig['name'] = $a_applications[$id]['name'];
+	$pconfig['extension'] = $a_applications[$id]['extension'];
+}
+
+if ($_POST) {
+
+	unset($input_errors);
+	$pconfig = $_POST;
+	
+	/* input validation */
+	$reqdfields = explode(" ", "extension");
+	$reqdfieldsn = explode(",", "Extension");
+	
+	do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
+
+	if (($_POST['extension'] && !asterisk_is_valid_extension($_POST['extension']))) {
+		$input_errors[] = "A valid extension must be entered.";
+	}
+
+	if (!$input_errors) {
+		$app = array();		
+		$app['name'] = $_POST['name'];
+		$app['extension'] = $_POST['extension'];	
+
+		if (isset($id) && $a_applications[$id]) {
+			$gm['uniqid'] = $a_applications[$id]['uniqid'];
+			$a_applications[$id] = $app;
+		 } else {
+			$app['uniqid'] = "APPLICATION-MAPPING-" . uniqid(rand());
+			$a_applications[] = $app;
+		}
+		
+		touch($d_extensionsconfdirty_path);
+		
+		write_config();
+		
+		header("Location: dialplan_applications.php");
+		exit;
+	}
+}
+?>
+<?php include("fbegin.inc"); ?>
+<?php if ($input_errors) print_input_errors($input_errors); ?>
+	<form action="dialplan_applications_edit.php" method="post" name="iform" id="iform">
+		<table width="100%" border="0" cellpadding="6" cellspacing="0">
+			<tr> 
+				<td width="20%" valign="top" class="vncellreq">Name</td>
+				<td width="80%" colspan="2" class="vtable">
+					<select name="name" class="formfld" id="name">
+					<? foreach ($applications as $appname => $appdesc) : ?>
+					<option value="<?=$appname;?>" <?
+					if ($appname == $pconfig['name'])
+						echo "selected"; ?>
+					><?=$appname;?></option>
+					<? endforeach; ?>
+					</select>
+					<br>
+					<span class="vexpl">
+						<ul>
+						<? foreach ($applications as $appname => $appdesc) : ?>
+							<li><strong><?=$appname;?></strong>&nbsp;<?=$appdesc;?></li>
+						<? endforeach; ?>
+						</ul>
+					</span>
+				</td>
+			</tr>
+			<tr> 
+				<td valign="top" class="vncell">Extension</td>
+				<td colspan="2" class="vtable">
+					<input name="extension" type="text" class="formfldreq" id="extension" size="40" value="<?=htmlspecialchars($pconfig['extension']);?>"> 
+					<br><span class="vexpl">Internal extension used to reach this call group.</span>
+				</td>
+			</tr>
+			<tr> 
+				<td valign="top">&nbsp;</td>
+				<td colspan="2">
+					<input name="Submit" type="submit" class="formbtn" value="Save">
+					<?php if (isset($id) && $a_applications[$id]): ?>
+					<input name="id" type="hidden" value="<?=$id;?>"> 
+					<?php endif; ?>
+				</td>
+			</tr>
+		</table>
+	</form>
+<?php include("fend.inc"); ?>
