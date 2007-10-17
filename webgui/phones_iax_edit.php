5939c217f1700a519ebad5c81297d56c05f9ea5e
Michael Iedema <mriedema@gmail.com>
2007-10-17 15:39:29 +0000

    refactored voicemailbox field into a guiconfig.inc function
    git-svn-id: https://wush.net/svn/askozia/trunk@231 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- webgui/phones_iax_edit.php
+++ webgui/phones_iax_edit.php
@@ -174,13 +174,7 @@ if ($_POST) {
                     <br><span class="vexpl">This account's password and authentication scheme.</span>
 				</td>
 			</tr>
-			<tr> 
-				<td valign="top" class="vncell">Voicemail Address</td>
-				<td colspan="2" class="vtable">
-					<input name="voicemailbox" type="text" class="formfld" id="voicemailbox" size="40" value="<?=htmlspecialchars($pconfig['voicemailbox']);?>"> 
-					<br><span class="vexpl">An e-mail address. If entered, voicemail will be enabled on this extension. Incoming messages will be sent to the given address.</span>
-				</td>
-			</tr>
+			<? display_call_notifications_editor($pconfig['voicemailbox'], 2); ?>
 			<? display_channel_language_selector($pconfig['language'], 2); ?>
 			<tr> 
 				<td valign="top" class="vncell">Qualify</td>
