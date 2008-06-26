e3d90cba9ddb6dce1a074286c464ff10b51270f0
kryysler <kryysler@c9cd69a0-eb2a-0410-8ff2-b9414780476a>
2008-06-26 08:06:11 +0000

    extracted the strings from advanced_isdn.php
    git-svn-id: https://wush.net/svn/askozia/trunk@581 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- webgui/advanced_isdn.php
+++ webgui/advanced_isdn.php
@@ -29,7 +29,7 @@
 	POSSIBILITY OF SUCH DAMAGE.
 */
 
-$pgtitle = array("Advanced", "ISDN");
+$pgtitle = array(gettext("Advanced"), gettext("ISDN"));
 require("guiconfig.inc");
 
 $isdnconfig = &$config['services']['isdn'];
@@ -49,17 +49,17 @@ if ($_POST) {
 
 	/* input validation */
 	$reqdfields = explode(" ", "nationalprefix internationalprefix");
-	$reqdfieldsn = explode(",", "National Prefix, International Prefix");
+	$reqdfieldsn = explode(",", gettext("National Prefix, International Prefix"));
 	
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
 
 	// is valid nationalprefix
 	if ($_POST['nationalprefix'] && !verify_is_numericint($_POST['nationalprefix'])) {
-		$input_errors[] = "A valid national prefix must be specified.";
+		$input_errors[] = gettext("A valid national prefix must be specified.");
 	}
 	// is valid internationalprefix
 	if ($_POST['internationalprefix'] && !verify_is_numericint($_POST['internationalprefix'])) {
-		$input_errors[] = "A valid international prefix must be specified.";
+		$input_errors[] = gettext("A valid international prefix must be specified.");
 	}
 
 	if (!$input_errors) {
@@ -98,31 +98,31 @@ if (file_exists($d_isdnconfdirty_path)) {
 <form action="advanced_isdn.php" method="post" name="iform" id="iform">
 	<table width="100%" border="0" cellpadding="6" cellspacing="0">
 		<tr>
-			<td width="20%" valign="top" class="vncell">National Prefix</td>
+			<td width="20%" valign="top" class="vncell"><?=gettext("National Prefix");?></td>
 			<td width="80%" class="vtable">
 				<input name="nationalprefix" type="text" class="formfld" id="nationalprefix" size="10" maxlength="5" value="<?=htmlspecialchars($pconfig['nationalprefix']);?>">
 			</td>
 		</tr>
 		<tr>
-			<td valign="top" class="vncell">International Prefix</td>
+			<td valign="top" class="vncell"><?=gettext("International Prefix");?></td>
 			<td class="vtable">
 				<input name="internationalprefix" type="text" class="formfld" id="internationalprefix" size="10" maxlength="5" value="<?=htmlspecialchars($pconfig['internationalprefix']);?>">
 			</td>
 		</tr>
 		<tr>
-			<td valign="top" class="vncell">Audio</td>
+			<td valign="top" class="vncell"><?=gettext("Audio");?></td>
 			<td class="vtable">
 				<input name="law" type="radio" value="alaw" 
-				<?php if ($pconfig['law'] == "alaw") echo "checked"; ?>>A-law
+				<?php if ($pconfig['law'] == "alaw") echo "checked"; ?>><?=gettext("A-law");?>
 				&nbsp;&nbsp;&nbsp;
 				<input name="law" type="radio" value="ulaw"
-				<?php if ($pconfig['law'] == "ulaw") echo "checked"; ?>>u-law
+				<?php if ($pconfig['law'] == "ulaw") echo "checked"; ?>><?=gettext("u-law");?>
 			</td>
 		</tr>
 		<tr> 
 			<td valign="top">&nbsp;</td>
 			<td>
-				<input name="Submit" type="submit" class="formbtn" value="Save">
+				<input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>">
 			</td>
 		</tr>
 	</table>
