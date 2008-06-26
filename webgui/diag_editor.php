2fb4a95ca154f34978a1514e0c0e3dc2b1c85927
kryysler <kryysler@c9cd69a0-eb2a-0410-8ff2-b9414780476a>
2008-06-26 08:39:45 +0000

    extracted strings from diag_editor.php
    git-svn-id: https://wush.net/svn/askozia/trunk@588 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- webgui/diag_editor.php
+++ webgui/diag_editor.php
@@ -29,7 +29,7 @@
 	POSSIBILITY OF SUCH DAMAGE.
 */
 
-$pgtitle = array("Diagnostics", "Editor");
+$pgtitle = array(gettext("Diagnostics"), gettext("Editor"));
 require("guiconfig.inc");
 
 $mode = "select";
@@ -41,7 +41,7 @@ if ($_POST) {
 	
 	if (isset($pconfig['open'])) {
 		if (!isset($pconfig['file']) || !file_exists($pconfig['file'])) {
-			$input_errors[] = "The specified file ({$pconfig['file']}) does not exist.";
+			$input_errors[] = sprintf(gettext("The specified file (%s) does not exist."), $pconfig['file']);
 		} else {
 			$mode = "edit";
 		}
@@ -58,7 +58,7 @@ if ($_POST) {
 		if ($needsremount) {
 			conf_mount_ro();
 		}
-		$savemsg = "File contents ({$pconfig['file']}) saved.";
+		$savemsg = sprintf(gettext("File contents (%s) saved."), $pconfig['file']);
 	}
 
 }
@@ -72,29 +72,29 @@ if ($_POST) {
 	<table width="100%" border="0" cellpadding="6" cellspacing="0">
 		<?php if ($mode == "edit"): ?>
 		<tr> 
-			<td width="22%" valign="top" class="vncellreq">File</td>
+			<td width="22%" valign="top" class="vncellreq"><?=gettext("File");?></td>
 			<td width="78%" class="vtable"><?=$mandfldhtml;?><input name="file" type="text" class="formfld" id="file" size="60" value="<?=htmlspecialchars($pconfig['file']);?>"></td>
 		</tr>
 		<tr> 
-			<td width="22%" class="vncellreq" valign="top">Contents</td>
+			<td width="22%" class="vncellreq" valign="top"><?=gettext("Contents");?></td>
 			<td width="78%" class="listr">
                 <textarea name="contents" cols="80" rows="21" id="contents" class="pre"><?=file_get_contents($pconfig['file']);?></textarea>
 			</td>
 		</tr>
 		<tr>
 			<td width="22%" valign="top">&nbsp;</td>
-			<td width="78%"> <input name="save" type="submit" class="formbtn" value="Save">
-				<input name="cancel" type="submit" class="formbtn" value="Cancel"> 
+			<td width="78%"> <input name="save" type="submit" class="formbtn" value="<?=gettext("Save");?>">
+				<input name="cancel" type="submit" class="formbtn" value="<?=gettext("Cancel");?>"> 
  			</td>
 		</tr>
 		<?php else: ?>
 		<tr> 
-			<td width="22%" valign="top" class="vncellreq">File</td>
+			<td width="22%" valign="top" class="vncellreq"><?=gettext("File");?></td>
 			<td width="78%" class="vtable"><?=$mandfldhtml;?><input name="file" type="text" class="formfld" id="file" size="60" value="<?=htmlspecialchars($pconfig['file']);?>"></td>
     	</tr>
 		<tr>
 			<td width="22%" valign="top">&nbsp;</td>
-			<td width="78%"> <input name="open" type="submit" class="formbtn" value="Open"> 
+			<td width="78%"> <input name="open" type="submit" class="formbtn" value="<?=gettext("Open");?>"> 
  			</td>
 		</tr>
 		<?php endif; ?>
