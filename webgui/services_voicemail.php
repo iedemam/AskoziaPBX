3e279975eddbafe8a16ca2821f5b6456c6ca8ed0
kryysler <kryysler@c9cd69a0-eb2a-0410-8ff2-b9414780476a>
2008-08-08 09:07:33 +0000

    - added option to define your own e-mail notification message for voicemail. updated locales.
    git-svn-id: https://wush.net/svn/askozia/trunk@776 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- webgui/services_voicemail.php
+++ webgui/services_voicemail.php
@@ -44,6 +44,9 @@ $pconfig['tls'] = $vmconfig['tls'];
 $pconfig['authtype'] = $vmconfig['authtype'];
 $pconfig['fromaddress'] = $vmconfig['fromaddress'];
 $pconfig['maillanguage'] = $vmconfig['maillanguage'];
+$pconfig['user_defined'] = isset($vmconfig['user_defined']);
+$pconfig['user_subject'] = base64_decode($vmconfig['user_subject']);
+$pconfig['user_body'] = base64_decode($vmconfig['user_body']);
 
 if ($_POST) {
 
@@ -51,11 +54,16 @@ if ($_POST) {
 	$pconfig = $_POST;
 
 	/* input validation */
-	$reqdfields = explode(" ", "host address");
-	$reqdfieldsn = explode(",", gettext("Host,E-mail Address"));
+	if ($_POST['user_defined']) {
+		$reqdfields = explode(" ", "host address user_subject user_body");
+		$reqdfieldsn = explode(",", gettext("Host,E-mail Address,Subject,Body"));
+	} else {
+		$reqdfields = explode(" ", "host address");
+		$reqdfieldsn = explode(",", gettext("Host,E-mail Address"));
+	}
 	
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
-	
+
 	if ($_POST['address'] && !verify_is_email_address($_POST['address'])) {
 		$input_errors[] = gettext("A valid e-mail address must be specified.");
 	}
@@ -82,6 +90,9 @@ if ($_POST) {
 		$vmconfig['authtype'] = verify_non_default($_POST['authtype'], $defaults['voicemail']['authtype']);
 		$vmconfig['fromaddress'] = $_POST['fromaddress'];
 		$vmconfig['maillanguage'] = $_POST['maillanguage'];
+		$vmconfig['user_defined'] = isset($_POST['user_defined']);
+		$vmconfig['user_subject'] = base64_encode($_POST['user_subject']);
+		$vmconfig['user_body'] = base64_encode($_POST['user_body']);
 		
 		write_config();
 		
@@ -96,7 +107,19 @@ if ($_POST) {
 	}
 }
 ?>
+
 <?php include("fbegin.inc"); ?>
+<script type="text/JavaScript">
+<!--
+	<?=javascript_userdefined_email_notification("functions");?>
+
+	jQuery(document).ready(function(){
+
+		<?=javascript_userdefined_email_notification("ready");?>
+	});
+
+//-->
+</script>
 <?php if ($input_errors) print_input_errors($input_errors); ?>
 <?php if ($savemsg) print_info_box($savemsg); ?>
 <form action="services_voicemail.php" method="post" name="iform" id="iform">
@@ -136,7 +159,7 @@ if ($_POST) {
 			<td valign="top" class="vncell"><?=gettext("Options");?></td>
 			<td class="vtable"> 
 				<input name="tls" type="checkbox" id="tls" value="yes" <?php if ($pconfig['tls']) echo "checked"; ?>>
-            	Account uses TLS<br>
+            			<?=gettext("Account uses TLS");?><br>
 				<span class="vexpl"><span class="red"><strong><?=gettext("Warning:");?></strong></span> <?=gettext("TLS certificates are currently not verified.");?></span>
 			</td>
         </tr>
@@ -167,6 +190,8 @@ if ($_POST) {
 				<? endforeach; ?>
 				</select>
 				<br><span class="vexpl"><?=gettext("E-mail notifications for new voicemail will be delivered in this language.");?></span>
+				<br><input name="user_defined" type="checkbox" id="user_defined" value="yes" <?php if ($pconfig['user_defined']) echo "checked"; ?>> <?=gettext("User defined e-mail text");?>
+				<? display_userdefined_email_notification($pconfig['user_subject'], $pconfig['user_body']); ?>
 			</td>
 		</tr>
 		<tr> 
