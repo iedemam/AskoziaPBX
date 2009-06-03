c796ad26224b95228f1c0e684f2842c7dda0afe7
Michael Iedema <mriedema@gmail.com>
2009-06-03 15:15:44 +0000

    - base system + kernel now less than 10MB total - modified firmware build.sh to make initramfs archives - stripped kernel configuration down
    git-svn-id: https://wush.net/svn/askozia/trunk@941 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- t2-base/target/pbx/build.sh
+++ t2-base/target/pbx/build.sh
@@ -18,35 +18,10 @@
 
 pkgloop
 
-echo_header "Finishing build."
-
-rm -f $build_toolchain/isofs*.txt 2> /dev/null
-
-if [ "$SDECFG_PKGFILE_TYPE" != "none" ]; then
-	echo_status "Creating package database ..."
-	admdir="build/${SDECFG_ID}/var/adm"
-	create_package_db $admdir $build_toolchain/pkgs \
-	                  $build_toolchain/pkgs/packages.db
-fi
+echo_header "Packaging build."
 
 if [ "$SDECFG_IMAGE" -a -e target/share/$SDECFG_IMAGE/build.sh ]; then
 	echo_status "Creating output image ..."
 	. target/share/$SDECFG_IMAGE/build.sh
 fi
 
-echo_status "Creating isofs.txt file .."
-(
-  if [ "$SDECFG_PKGFILE_TYPE" != "none" ]; then
-    cat <<-EOT
-	DISK1	$admdir/cache/		${SDECFG_SHORTID}/info/cache/
-	DISK1	$admdir/dependencies/	${SDECFG_SHORTID}/info/dependencies/
-	DISK1	$admdir/descs/		${SDECFG_SHORTID}/info/descs/
-	DISK1	$admdir/flists/		${SDECFG_SHORTID}/info/flists/
-	DISK1	$admdir/md5sums/	${SDECFG_SHORTID}/info/md5sums/
-	DISK1	$admdir/packages/	${SDECFG_SHORTID}/info/packages/
-	EVERY	$build_toolchain/pkgs/packages.db	${SDECFG_SHORTID}/pkgs/packages.db
-	SPLIT	$build_toolchain/pkgs/	${SDECFG_SHORTID}/pkgs/
-EOT
-  fi
-  cat $build_toolchain/isofs_*.txt 2>/dev/null
-) > $build_toolchain/isofs.txt
--- t2-base/target/share/firmware/build.sh
+++ t2-base/target/share/firmware/build.sh
@@ -14,95 +14,95 @@
 #
 #Description: WebGUI installable firmware image
 
-set -e
-imagelocation="$build_toolchain/rootfs"	# where the roofs is prepared
-
 . $base/misc/target/functions.in
 
+set -e
+
+# set initramfs preparation directory
+imagelocation="$build_toolchain/initramfs"
+
+echo "Preparing initramfs image from build result ..."
+
+rm -rf $imagelocation{,.cpio,.igz}
 mkdir -p $imagelocation ; cd $imagelocation
-# just in case it is still there from the last run
-(umount proc ; umount dev) 2>/dev/null
 
-echo "Creating root file-system file lists ..."
-f=" $pkg_filter "
-for pkg in `grep '^X ' $base/config/$config/packages | cut -d ' ' -f 5`; do
-	# include the package?
-	if [ "${f/ $pkg /}" == "$f" ] ; then
-		cut -d ' ' -f 2 $build_root/var/adm/flists/$pkg 2>/dev/null || true
-	fi
-done > ../files-wanted
-unset f
-[ "$filter_hook" ] && "$filter_hook" ../files-wanted
-sort -u ../files-wanted > x ; mv -f x ../files-wanted
+find $build_root -printf "%P\n" | sed '
 
-# for rsync with --delete we can not use file lists, since rsync does not
-# delete in that mode - instead we need to generate a negative list
-time find $build_root -mount -wholename $build_root/TOOLCHAIN -prune -o -printf '%P\n' |
-	sort -u > ../files-all
-# the difference
-diff -u ../files-all ../files-wanted |
-sed -n -e '/var\/adm\/olist/d' -e '/var\/adm\/logs/d' \
-       -e '/var\/adm\/dep-debug/d' -e '/var\/adm\/cache/d' -e 's/^-//p' > ../files-exclude
-echo "TOOLCHAIN
-proc/*
-dev/*
-*/share/doc/*
-var/adm/olist
-var/adm/logs
-var/adm/dep-debug
-var/adm/cache" >> ../files-exclude
+# stuff we never need
 
-echo "Syncing root file-system (this may take some time) ..."
-[ -e $imagelocation/bin ] && v="-v" || v=""
-time rsync -artH $v --devices --specials --delete --delete-excluded \
-     --exclude-from ../files-exclude $build_root/ $imagelocation/
-rm ../files-{wanted,all,exclude}
+/^TOOLCHAIN/	d;
+/^var\/adm/	d;
 
-echo "Overlaying root file-system with target defined files ..."
-copy_and_parse_from_source $base/target/share/firmware/rootfs $imagelocation
-[ -e $base/target/$target/rootfs ] &&
-	copy_and_parse_from_source $base/target/$target/rootfs $imagelocation
+/\/include/	d;
+/\/src/		d;
+/\.a$/		d;
+/\.o$/		d;
+/\.old$/	d;
 
-[ "$inject_hook" ] && "$inject_hook"
+/\/games/	d;
+/\/local/	d;
+/^boot/		d;
 
-if false; then
-echo "Running ldconfig and other postinstall scripts ..."
-mount /dev dev --bind
-mount none proc -t proc
-for x in sbin/ldconfig etc/postinstall.d/*; do
-	cat > $$.sh <<-EOT
-		. /etc/profile
-		$x
-EOT
-	chmod +x $$.sh
-	case $x in
-		*/scrollkeeper) echo "$x left out" ;;
-		*) chroot . /bin/sh -c ". /etc/profile; /$x" && true ;;
-	esac
-	rm $$.sh
-done
-umount proc
-umount dev
-fi
+# # stuff that would be nice - but is huge and only documentation
+/\/man/		d;
+/\/doc/		d;
 
-echo "Compression root file-system (this may take some time) ..."
-time find . | cpio -o -H newc | gzip -c9 > $imagelocation/../initrd.img
-du -sh $imagelocation/../initrd.img
+# /etc noise
+/^etc\/stone.d/	d;
+/^etc\/cron.d/	d;
+/^etc\/init.d/	d;
 
-# For each available kernel:
-#
-mkdir -p $imagelocation/../boot/
-for x in `egrep 'X .* KERNEL .*' $base/config/$config/packages |
-          cut -d ' ' -f 5`; do
- kernel=${x/_*/}
- for moduledir in `grep lib/modules $build_root/var/adm/flists/$kernel |
-                   cut -d ' ' -f 2 | cut -d / -f 1-3 | uniq`; do
-  kernelver=${moduledir/*\/}
-  initrd="initrd-$kernelver.img"
-  kernelimg=`ls $build_root/boot/vmlinu?_$kernelver`
-  kernelimg=${kernelimg##*/}
+/^etc\/skel/	d;
+/^etc\/opt/	d;
+/^etc\/conf/	d;
+/^etc\/rc.d/	d;
 
-  cp $build_root/boot/vmlinu?_$kernelver $imagelocation/../boot/
-  cp $imagelocation/../initrd.img $imagelocation/../boot/$initrd
- done
-done
+/^opt/		d;
+
+/^\/man\//	d;
+
+# /usr/share
+/usr\/share\/aclocal/	d;
+/usr\/share\/autoconf/	d;
+/usr\/share\/automake/	d;
+/usr\/share\/bison/		d;
+/usr\/share\/dict/		d;
+/usr\/share\/gettext/	d;
+/usr\/share\/info/		d;
+/usr\/share\/libtool/	d;
+/usr\/share\/misc/		d;
+
+# terminfo
+/terminfo\/a\/ansi$/	{ p; d; }
+/terminfo\/l\/linux$/	{ p; d; }
+/terminfo\/x\/xterm$/	{ p; d; }
+/terminfo\/n\/nxterm$/	{ p; d; }
+/terminfo\/x\/xterm-color$/	{ p; d; }
+/terminfo\/x\/xterm-8bit$/	{ p; d; }
+/terminfo\/x\/screen$/	{ p; d; }
+/terminfo\/v\/vt100$/	{ p; d; }
+/terminfo\/v\/vt200$/	{ p; d; }
+/terminfo\/v\/vt220$/	{ p; d; }
+/terminfo/	d;
+
+' > tar.input
+
+copy_with_list_from_file $build_root . $PWD/tar.input
+rm tar.input
+
+echo "Preparing initramfs image from target defined files ..."
+copy_from_source $base/target/$target/rootfs .
+
+echo "Creating links for identical files ..."
+link_identical_files
+
+echo "Creating initramfs image ..."
+find . | cpio -H newc -o > ../initramfs.cpio
+
+echo "Compressing initramfs image ..."
+cd ..
+cat initramfs.cpio | gzip > initramfs.igz
+
+du -sh $imagelocation{,.cpio,.igz}
+
+echo "The image is located at $imagelocation.igz."
