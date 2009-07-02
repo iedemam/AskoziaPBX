899ab629f422898678777b48b92d7d23f8afb76f
Michael Iedema <mriedema@gmail.com>
2009-07-02 12:19:59 +0000

    - index.php fixed: - column width expanded so all fields fit - memory usage fixed - uptime display fixed
    git-svn-id: https://wush.net/svn/askozia/trunk@1019 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- package/overlays/pbx/rootfs/usr/www/index.php
+++ package/overlays/pbx/rootfs/usr/www/index.php
@@ -46,112 +46,86 @@ if ($_POST) {
 ?>
 <?php include("fbegin.inc"); ?>
 <form action="" method="POST">
-            <table width="100%" border="0" cellspacing="0" cellpadding="0">
-              <tr align="center" valign="top"> 
-                <td height="10" colspan="2">&nbsp;</td>
-              </tr>
-              <tr align="center" valign="top"> 
-                <td height="170" colspan="2"><img src="logobig.png"></td>
-              </tr>
-              <tr> 
-                <td colspan="2" class="listtopic"><?=gettext("System Information");?></td>
-              </tr>
-              <tr> 
-                <td width="25%" class="vncellt"><?=gettext("Name");?></td>
-                <td width="75%" class="listr">
-                  <?php echo $config['system']['hostname'] . "." . $config['system']['domain']; ?>
-                </td>
-              </tr>
-              <tr> 
-                <td width="25%" valign="top" class="vncellt"><?=gettext("Version");?></td>
-                <td width="75%" class="listr"> <strong> 
-                  <?php readfile("/etc/version"); ?>
-                  </strong><br>
-                  <?=gettext("built on");?> 
-                  <?php readfile("/etc/version.buildtime"); ?>
-                </td>
-              </tr>
-              <tr> 
-                <td width="25%" class="vncellt"><?=gettext("Platform");?></td>
-                <td width="75%" class="listr"> 
-                  <?=htmlspecialchars($g['platform']);?>
-                </td>
-              </tr>
-              <tr> 
-                <td width="25%" class="vncellt"><?=gettext("Uptime");?></td>
-                <td width="75%" class="listr"> 
-                  <?php
-				  	exec("/sbin/sysctl -n kern.boottime", $boottime);
-					preg_match("/sec = (\d+)/", $boottime[0], $matches);
-					$boottime = $matches[1];
-					$uptime = time() - $boottime;
-					
-					if ($uptime > 60)
-						$uptime += 30;
-					$updays = (int)($uptime / 86400);
-					$uptime %= 86400;
-					$uphours = (int)($uptime / 3600);
-					$uptime %= 3600;
-					$upmins = (int)($uptime / 60);
-					
-					$uptimestr = "";
-					if ($updays > 1)
-						$uptimestr .= sprintf(gettext("%s days, "), $updays);
-					else if ($updays > 0)
-						$uptimestr .= "1 day, ";
-					$uptimestr .= sprintf("%02d:%02d", $uphours, $upmins);
-					echo htmlspecialchars($uptimestr);
-				  ?>
-                </td>
-              </tr><?php if ($config['lastchange']): ?>
-              <tr> 
-                <td width="25%" class="vncellt"><?=gettext("Last Config Change");?></td>
-                <td width="75%" class="listr"> 
-                  <?=htmlspecialchars(date("D M j G:i:s T Y", $config['lastchange']));?>
-                </td>
-              </tr><?php endif; ?>
-			
-			<? pbx_get_active_calls(&$active_calls, &$active_channels, &$channel_list); ?>
-			<tr> 
-				<td width="25%" class="vncellt"><?=gettext("Active Calls");?></td>
-				<td width="75%" class="listr"><?=$active_calls;?>&nbsp;</td>
-			</tr>
-			<tr> 
-				<td width="25%" class="vncellt"><?=gettext("Active Channels");?></td>
-				<td width="75%" class="listr"><?=$active_channels;?>&nbsp;</td>
-			</tr>
-			
-			  <tr> 
-                <td width="25%" class="vncellt"><?=gettext("Memory Usage");?></td>
-                <td width="75%" class="listr">
-<?php
-
-exec("/sbin/sysctl -n vm.stats.vm.v_active_count vm.stats.vm.v_inactive_count " .
-	"vm.stats.vm.v_wire_count vm.stats.vm.v_cache_count vm.stats.vm.v_free_count vm.stats.vm.v_page_size", $memory);
-
-$totalMem = $memory[0] + $memory[1] + $memory[2] + $memory[3] + $memory[4];
-$freeMem = $memory[4];
-$usedMem = $totalMem - $freeMem;
-$memUsage = round(($usedMem * 100) / $totalMem, 0);
-$pagekbytes = $memory[5] / 1024;
-
-$usageTitle = $usedMem * $pagekbytes . " / " . $totalMem * $pagekbytes . " kBytes";
-		  
-echo " <img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle' title='$usageTitle'>";
-echo "<img src='bar_blue.gif' height='15' width='" . $memUsage . "' border='0' align='absmiddle' title='$usageTitle'>";
-echo "<img src='bar_gray.gif' height='15' width='" . (100 - $memUsage) . "' border='0' align='absmiddle' title='$usageTitle'>";
-echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle' title='$usageTitle'> ";
-echo $memUsage . "%";
-?>
-                </td>
-              </tr>
-              <tr> 
-                <td width="25%" class="vncellt" valign="top"><?=gettext("Notes");?></td>
-                <td width="75%" class="listr">
-                  <textarea name="notes" cols="75" rows="7" id="notes" class="notes"><?=htmlspecialchars(base64_decode($config['system']['notes']));?></textarea><br>
-                  <input name="Submit" type="submit" class="formbtns" value="<?=gettext("Save");?>">
-                </td>
-              </tr>
-            </table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr align="center" valign="top"> 
+		<td height="10" colspan="2">&nbsp;</td>
+	</tr>
+	<tr align="center" valign="top"> 
+		<td height="170" colspan="2"><img src="logobig.png"></td>
+	</tr>
+	<tr>
+		<td colspan="2" class="listtopic"><?=gettext("System Information");?></td>
+	</tr>
+	<tr>
+		<td width="30%" class="vncellt"><?=gettext("Name");?></td>
+		<td width="70%" class="listr">
+			<? echo $config['system']['hostname'] . "." . $config['system']['domain']; ?>
+		</td>
+	</tr>
+	<tr>
+		<td valign="top" class="vncellt"><?=gettext("Version");?></td>
+		<td class="listr"><strong><?
+			readfile("/etc/version");
+			?></strong><br><?
+			echo gettext("built on");
+			readfile("/etc/version.buildtime");
+		?></td>
+	</tr>
+	<tr>
+		<td class="vncellt"><?=gettext("Platform");?></td>
+		<td class="listr"><?=htmlspecialchars($g['platform']);?></td>
+	</tr>
+	<tr>
+		<td class="vncellt"><?=gettext("Uptime");?></td>
+		<td class="listr"><?
+			exec("/usr/bin/uptime", $ut);
+			$start = strpos($ut[0], "up") + 2;
+			$end = strpos($ut[0], ",");
+			$ut = substr($ut[0], $start, $end-$start);
+			echo htmlspecialchars($ut);
+		?></td>
+	</tr><?
+if ($config['lastchange']) {
+	?><tr>
+		<td class="vncellt"><?=gettext("Last Config Change");?></td>
+		<td class="listr"> 
+			<?=htmlspecialchars(date("D M j G:i:s T Y", $config['lastchange']));?>
+		</td>
+	</tr><?
+}
+	pbx_get_active_calls(&$active_calls, &$active_channels, &$channel_list);
+	?><tr> 
+		<td class="vncellt"><?=gettext("Active Calls");?></td>
+		<td class="listr"><?=$active_calls;?>&nbsp;</td>
+	</tr>
+	<tr> 
+		<td class="vncellt"><?=gettext("Active Channels");?></td>
+		<td class="listr"><?=$active_channels;?>&nbsp;</td>
+	</tr>
+	<tr> 
+		<td class="vncellt"><?=gettext("Memory Usage");?></td>
+		<td class="listr"><?
+			exec("/usr/bin/free", $memory);
+			$memory = preg_split("/\s+/", $memory[1]);
+			$totalMem = $memory[2];
+			$freeMem = $memory[4];
+			$usedMem = $totalMem - $freeMem;
+			$memUsage = round(($usedMem * 100) / $totalMem, 0);
+			$usageTitle = $usedMem . " / " . $totalMem . " kBytes";
+			echo "<img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle' title='$usageTitle'>";
+			echo "<img src='bar_blue.gif' height='15' width='" . $memUsage . "' border='0' align='absmiddle' title='$usageTitle'>";
+			echo "<img src='bar_gray.gif' height='15' width='" . (100 - $memUsage) . "' border='0' align='absmiddle' title='$usageTitle'>";
+			echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle' title='$usageTitle'> ";
+			echo $memUsage . "%";
+		?></td>
+	</tr>
+	<tr> 
+		<td class="vncellt" valign="top"><?=gettext("Notes");?></td>
+		<td class="listr">
+			<textarea name="notes" cols="65" rows="7" id="notes" class="notes"><?=htmlspecialchars(base64_decode($config['system']['notes']));?></textarea><br>
+			<input name="Submit" type="submit" class="formbtns" value="<?=gettext("Save");?>">
+		</td>
+	</tr>
+</table>
 </form>
-            <?php include("fend.inc"); ?>
+<?php include("fend.inc"); ?>
