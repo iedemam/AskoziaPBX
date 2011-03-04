7c7a35b1492e70f4977e320982971f52446e6944
Michael Iedema <mriedema@gmail.com>
2011-03-04 10:58:09 +0000

    merging r1856 from 2.0 branch into trunk
    git-svn-id: https://wush.net/svn/askozia/trunk@1857 c9cd69a0-eb2a-0410-8ff2-b9414780476a

--- package/overlays/pbx/rootfs/usr/www/index.php
+++ package/overlays/pbx/rootfs/usr/www/index.php
@@ -53,7 +53,7 @@ if ($_POST) {
 		<td height="10" colspan="2">&nbsp;</td>
 	</tr>
 	<tr align="center" valign="top"> 
-		<td height="170" colspan="2"><img src="logobig.png"></td>
+		<td height="170" colspan="2"><img src="platform.png"></td>
 	</tr>
 	<tr>
 		<td colspan="2" class="listtopic"><?=gettext("System Information");?></td>
